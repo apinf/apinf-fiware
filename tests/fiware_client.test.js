a9303b2eecf7de17ecef3ed454a35094aa952e55
yannunes <yannunes@gmail.com>
2017-08-17 01:41:12 -0300

    Adds unit testing for the client

--- tests/fiware_client.test.js
+++ tests/fiware_client.test.js
@@ -1,6 +1,5 @@
 import nock from 'nock'
-import { chai } from 'meteor/practicalmeteor:chai'
-import fiwareClient from '../fiware_client.js'
+import { expect } from 'meteor/practicalmeteor:chai'
 
 // Defines root URL
 const rootUrl = `https://account.lab.fiware.org`
@@ -14,8 +13,27 @@ const clientId = 'bd78834613d94aaf939646f9014a0894'
 // Defining secret for testing
 const secret = 'be2d674d4d0f4e97b10d3c63e78fd06a'
 
-describe('should pass', function() {
-  it('should pass', function(done) {
+
+// Get Url for mocking results
+const getUrl = '/oauth2/authorize?' +
+               'response_type=code&' +
+               'client_id=bd78834613d94aaf939646f9014a0894&' +
+               'redirect_uri=http://localhost:3000&' +
+               'state=xxx'
+
+const mockResultsForLoginRequest = '-Ve4fzRMP8KhMvz4EoSUiNvNrZnG_oGxITcscV1XYTj'
+
+describe('requestCredential function', function() {
+  it('should be a function', function(done) {
+    var err = null
+    try {
+      expect(typeof Fiware.requestCredential).to.be.equal('function')
+    } catch(e) {
+      err = e
+    }
+    done(err)
+  })
+  it('should return valid credentialToken', function(done) {
     this.timeout(50000)
     // Defining mock url for login
 
@@ -28,27 +46,14 @@ describe('should pass', function() {
       redirectURI: 'http://localhost:3000'
     })
 
-    // Get Url for mocking results
-    const getUrl = '/oauth2/authorize?' +
-                   'response_type=code&' +
-                   'client_id=bd78834613d94aaf939646f9014a0894&' +
-                   'redirect_uri=http://localhost:3000&' +
-                   'state=xxx'
-
-    const mockResultsForLoginRequest = {
-
-    }
-
-    // Mocks URL Result
-    // loginUrlMock
-    //   .filteringPath(/state=[^&]*/g, 'state=XXX')
-    //   .get(getUrl)
-    //   .reply(200, 'user');
-
     Fiware.requestCredential(function(res) {
-      console.log('RESULT')
-      console.log(res)
-      done()
+      var err = null
+      try {
+        expect(res.length).to.be.equal(Random.secret().length)
+      } catch(e) {
+        err = e
+      }
+      done(err)
     })
   })
 })
