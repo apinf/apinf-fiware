37d42008a78d966936f9b99c3ca8f739d3962b45
yannunes <yannunes@gmail.com>
2017-08-16 22:51:08 -0300

    Adds configuration settings to test case

--- tests/fiware_client.test.js
+++ tests/fiware_client.test.js
@@ -1,5 +1,30 @@
+import { nock } from 'meteor/hharnisc:meteor-nock'
+import { chai } from 'meteor/practicalmeteor:chai'
+import fiwareClient from '../fiware_client.js'
+
+// Defining mock url for login
+const loginUrlMock = `https://account.lab.fiware.org`
+
+// Defining clientID for testing
+const clientId = 'bd78834613d94aaf939646f9014a0894'
+
+// Defining secret for testing
+const secret = 'be2d674d4d0f4e97b10d3c63e78fd06a'
+
+
 describe('should pass', function() {
   it('should pass', function(done) {
-    done()
+    console.log(fiwareClient)
+    ServiceConfiguration.configurations.insert({
+      service: 'fiware',
+      clientId: clientId,
+      secret: secret,
+      rootURL: loginUrlMock,
+      redirectURI: 'http://localhost:3000'
+    })
+    Fiware.requestCredential({}, function(res) {
+      console.log(res)
+      done()
+    })
   })
 })
