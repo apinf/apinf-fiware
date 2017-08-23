c1e62be3903ae905cc53733083c914cfc672f6b4
yannunes <yannunes@gmail.com>
2017-08-23 19:19:42 -0300

    Fix mock server url

--- tests/mock.data.js
+++ tests/mock.data.js
@@ -1,6 +1,6 @@
 const requestInfo = {
   redirectURI: 'http://localhost:3000/_oauth/fiware',
-  rootUrl: 'https://localhost:3010',
+  rootUrl: 'http://localhost:3010',
   clientId: 'bd78834613d94aaf939646f9014a0894',
   secret: 'be2d674d4d0f4e97b10d3c63e78fd06a',
   authHeader: 'YmQ3ODgzNDYxM2Q5NGFhZjkzOTY0NmY5MDE0YTA4OTQ6YmUyZDY3NGQ0ZDBmNGU5N2IxMGQzYzYzZTc4ZmQwNmE==',
