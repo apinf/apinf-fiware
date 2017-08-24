6dd4f50f74da956391b15931f6f84b7f72147033
yannunes <yannunes@gmail.com>
2017-08-23 21:46:24 -0300

    Remove debug logs

--- tests/popup-stub/express-stub.js
+++ tests/popup-stub/express-stub.js
@@ -6,19 +6,13 @@ const port = '3010'
 let app = express()
 
 app.get('/oauth2/authorize', (req, res) => {
-  console.log(req.query)
-  console.log(req.query.response_type == 'code')
-  console.log(req.query.client_id == clientId)
-  console.log(req.query.state == state)
+
   if (
     req.query.response_type == 'code' &&
     req.query.client_id == data.requestInfo.clientId &&
-    req.query.state == data.requestInfo.state
+    req.query.state.state == data.requestInfo.state.state
   ) {
-    res.json({
-      state: state,
-      code: 'rZzOAjVFIDFHlyxVvmUWZl62hCcou7'
-    })
+    res.json(data.loginMock.responseBody)
   }
 })
 
