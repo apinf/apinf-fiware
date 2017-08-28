4b8d4d9e6d2b6fb1e9bec4189a22d2abb06f40c1
yannunes <yannunes@gmail.com>
2017-08-28 20:35:31 -0300

    Test toBase64 function in config file

--- tests/fiware_server.test.js
+++ tests/fiware_server.test.js
@@ -6,7 +6,7 @@ import nock from 'nock'
 import { expect } from 'meteor/practicalmeteor:chai'
 
 // Import all config objects
-import * as config from '../config.js'
+import config from '../config.js'
 
 // Basic request info that all mocks use
 import { requestInfo, accessTokenMock, getAccountMock } from './mock.data.js'
@@ -96,6 +96,49 @@ describe('config file', function() {
     // Test done
     done(err)
   })
+
+  describe('toBase64 property', function() {
+    it('should be a function', function(done) {
+      // Error variable
+      let err = null
+
+      // Try/Catch statement throws error if toBase64 is not a function
+      try {
+        // Expects to be function. If not, throw error
+        expect(typeof config.toBase64).to.be.equal('function')
+      } catch(e) {
+        // Catchs thrown error and sets it to error variable
+        err = e
+      }
+
+      // Test done
+      done(err)
+    })
+
+    it('should encode string to base 64', function(done) {
+      // Error variable
+      let err = null
+
+      // String to be enconded on test
+      const stringToTest = 'string'
+
+      // Encoded string to be compared to function result
+      const base64OfStringToTest = 'c3RyaW5n'
+
+      // Try/Catch statement throws error if string don't match
+      try {
+        // Expects to be equal. If not, throw error
+        expect(config.toBase64(stringToTest)).to.be.equal(base64OfStringToTest)
+      } catch(e) {
+        // Catchs thrown error and sets it to error variable
+        err = e
+      }
+
+      // Test done
+      done(err)
+    })
+  })
+
 })
 
 describe('allowedFields', function() {
