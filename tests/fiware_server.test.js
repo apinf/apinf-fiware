d0392b09afd385e0d2f831cd2e173b6a0e758ce5
yannunes <yannunes@gmail.com>
2017-08-28 20:28:10 -0300

    Test config file along with server testing

--- tests/fiware_server.test.js
+++ tests/fiware_server.test.js
@@ -5,6 +5,9 @@ global.Buffer = global.Buffer || require("buffer").Buffer
 import nock from 'nock'
 import { expect } from 'meteor/practicalmeteor:chai'
 
+// Import all config objects
+import * as config from '../config.js'
+
 // Basic request info that all mocks use
 import { requestInfo, accessTokenMock, getAccountMock } from './mock.data.js'
 
@@ -75,6 +78,26 @@ describe('retrieveCredential', function() {
   })
 
 })
+
+describe('config file', function() {
+  it('should be an object', function(done) {
+    // Error variable
+    let err = null
+
+    // Try/Catch statement throws error if config is an array
+    try {
+      // Expects to be object. If not, throw error
+      expect(config).to.be.an('object')
+    } catch(e) {
+      // Catchs thrown error and sets it to error variable
+      err = e
+    }
+
+    // Test done
+    done(err)
+  })
+})
+
 describe('allowedFields', function() {
   it('should be an array', function(done) {
     // Error variable
