6803fee1119cf466b2cda46c67a414965ff633b9
yannunes <yannunes@gmail.com>
2017-08-29 21:24:23 -0300

    Defines statePrams function on the server side

--- config.js
+++ config.js
@@ -1,9 +1,32 @@
 /**
-  * Converts a string to base64
-  * @param   {String} string       String to be converted
-  * @return  {String}              Converted string
- */
+* Converts a string to base64
+* @param   {String} string       String to be converted
+* @return  {String}              Converted string
+*/
 const toBase64 = (string) => new Buffer(string).toString('base64')
+
+/**
+ * This is an actual copied and paste function from the Meteor OAuth package.
+ * This function is only defined on the client side for the OAuth and, since the
+ * config.test.js is running on the server side, it needs to be defined here to
+ * complete all testing.
+ */
+const getStateParam = (loginStyle, credentialToken, redirectUrl) => {
+  var state = {
+    loginStyle: loginStyle,
+    credentialToken: credentialToken,
+    isCordova: Meteor.isCordova
+  };
+
+  if (loginStyle === 'redirect')
+    state.redirectUrl = redirectUrl || ('' + window.location);
+
+  // Encode base64 as not all login services URI-encode the state
+  // parameter when they pass it back to us.
+  // Use the 'base64' package here because 'btoa' isn't supported in IE8/9.
+  return toBase64(JSON.stringify(state));
+};
+
 
 // Hardcoded hashes
 const hashs = {
@@ -14,11 +37,11 @@ const hashs = {
 const endpoints = {
 
   // Function that builds loginUrl dynamicaly with the config object
-  buildLoginUrl: (config) => (
+  buildLoginUrl: (config, loginStyle, credentialToken) => (
     config.rootURL + '/oauth2/authorize?response_type=code' +
     '&client_id=' + config.clientId +
-    '&redirect_uri=' + config.redirectURI +
-    '&state=' + OAuth._stateParam(loginStyle, credentialToken)
+    '&redirect_uri=' + config.redirectURI + '&state=' +
+    (OAuth._stateParam? OAuth._stateParam(loginStyle, credentialToken) : getStateParam(loginStyle, credentialToken))
   ),
 
   // Function that builds getTokens Url dynamicaly with the config object
