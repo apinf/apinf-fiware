bb7d20f991f0bec7c80892bca1c352605f3e44a2
yannunes <yannunes@gmail.com>
2017-08-29 21:23:59 -0300

    Fix wrong state parameter mock

--- tests/mock.data.js
+++ tests/mock.data.js
@@ -4,7 +4,7 @@ const requestInfo = {
   clientId: 'bd78834613d94aaf939646f9014a0894',
   secret: 'be2d674d4d0f4e97b10d3c63e78fd06a',
   authHeader: 'YmQ3ODgzNDYxM2Q5NGFhZjkzOTY0NmY5MDE0YTA4OTQ6YmUyZDY3NGQ0ZDBmNGU5N2IxMGQzYzYzZTc4ZmQwNmE=',
-  state: 'eyJsb2dpblN0eWxlIjoicG9wdXAiLCJjcmVkZW50aWFsVG9rZW4iOiJSOWpwQkJ4WmNoek9sTnp5RDI4dUxORXM4N2VYWXVNQUFlbFR0cklxeTdqIiwiaXNDb3Jkb3ZhIjpmYWxzZX0='
+  state: 'eyJsb2dpblN0eWxlIjoicG9wdXAiLCJjcmVkZW50aWFsVG9rZW4iOiJYR2U3SmhEYjBQOEVNdHMwNHpLaklTYVFfX3NMVW5uYTdQWE9mSTVnR3o4IiwiaXNDb3Jkb3ZhIjpmYWxzZX0='
 }
 
 const configResponse = {
